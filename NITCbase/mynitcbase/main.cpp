62939e0e17a3b9ded3050a16962d4b655f437f9d
sreeharij <sreeharij2003@gmail.com>
2024-12-11 15:32:31 +0530

    finished stage 2

--- NITCbase/mynitcbase/main.cpp
+++ NITCbase/mynitcbase/main.cpp
@@ -1,4 +1,4 @@
-#include "Buffer/StaticBuffer.h"
+#include "Buffer/BlockBuffer.h"
 #include "Cache/OpenRelTable.h"
 #include "Disk_Class/Disk.h"
 #include "FrontendInterface/FrontendInterface.h"
@@ -7,25 +7,32 @@ using std::cout;
 using std::endl;
 
 int main(int argc, char *argv[]) {
-  /* Initialize the Run Copy of Disk */
-  Disk disk_run;  
+  Disk disk_run;
 
-  unsigned char buffer[BLOCK_SIZE];
-  Disk::readBlock(buffer,7000);
-  char message[] = "hello";
-  memcpy(buffer+20,message,6);
-  Disk::writeBlock(buffer,7000);
+  RecBuffer relCatBuffer(RELCAT_BLOCK);
+  RecBuffer attrCatBuffer(ATTRCAT_BLOCK);
 
-  unsigned char buffer2[BLOCK_SIZE];
-  char message2[6];
-  Disk::readBlock(buffer2,7000);
-  memcpy(message2,buffer2+20,6);
-  cout<<message2<<endl;
-  
+  HeadInfo relCatHeader;
+  HeadInfo attrCatHeader;  
 
-  // StaticBuffer buffer;
-  // OpenRelTable cache;
+  relCatBuffer.getHeader(&relCatHeader);
+  attrCatBuffer.getHeader(&attrCatHeader);
 
-  // return FrontendInterface::handleFrontend(argc, argv);
+  for(int i=0;i<relCatHeader.numEntries;i++){
+    Attribute relCatRecord[RELCAT_NO_ATTRS]; //RELCAT_NO_ATTRS = 6 is defined since 6 attributes for a record in record catalog
+    relCatBuffer.getRecord(relCatRecord,i);
+
+    printf("Relation: %s\n", relCatRecord[RELCAT_REL_NAME_INDEX].sVal); //RELCAT_REL_NAME_INDEX = 0 defined for relation name
+    for(int j=0;j<attrCatHeader.numEntries;j++){
+      Attribute attrCatRecord[ATTRCAT_NO_ATTRS];
+      attrCatBuffer.getRecord(attrCatRecord,j);
+
+      if(strcmp(attrCatRecord[ATTRCAT_REL_NAME_INDEX].sVal,relCatRecord[RELCAT_REL_NAME_INDEX].sVal) == 0){
+        const char *attrType = attrCatRecord[ATTRCAT_ATTR_TYPE_INDEX].nVal == NUMBER ? "NUM" : "STR";
+        printf(" %s: %s\n",attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal,attrType);
+      }
+    }
+    printf("\n");
+  }
   return 0;
 }
